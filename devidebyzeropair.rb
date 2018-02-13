e53ae5bc216074a5573cd3c7e2a977935060141e
anna.kiuber <anna.kiuber@gmail.com>
2018-02-13 12:50:48 -0500

    devidebyzero

---
+++ devidebyzeropair.rb
@@ -0,0 +1,9 @@
+#devidingbyzero
+def devisionbyzero(number, other)
+	if other == 0
+		puts "retard strength activate"
+	number / other
+end
+
+puts devisionbyzero(25, 32)
+puts devisionbyzero(87, 98)
